790efd990d5fe0702e0dbd3b85eee13e247baeba
Eugene Viktorov <jekafromua@gmail.com>
2015-01-17 00:51:50 +0200

    pybursa_deploy

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -96,8 +96,19 @@ ALLOWED_HOSTS = ['*']
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+....
+
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+    root('templates'),
 )
 
 STATIC_ROOT = 'staticfiles'
