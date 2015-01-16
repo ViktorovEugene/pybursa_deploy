03d5dc5f63c32ab0c7e86c8baa41b41191d28396
Eugene Viktorov <jekafromua@gmail.com>
2015-01-16 23:43:48 +0200

    pybursa_deploy

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -3,9 +3,6 @@ from django.views.generic import TemplateView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from pybursa.views import DelationView
-
-
 urlpatterns = patterns('',
 
     url(r'^$', TemplateView.as_view(template_name='pybursa/base.html'), name='home'),
